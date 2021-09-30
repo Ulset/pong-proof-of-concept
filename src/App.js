6199d8df9f38e560d7f23095c380a0699e8481d6
Sander Ulset <sanderulset1@gmail.com>
2021-10-01 00:13:25 +0200

    game logic funker

--- src/App.js
+++ src/App.js
@@ -6,22 +6,26 @@ function App() {
     const y_length = 8
     const x_length = 8
 
-    //Where the ball is heading
-    const [y_axis, setY_axis] = useState(4);
+    //Position of the ball
+    const [y_axis, setY_axis] = useState(2);
     const [x_axis, setX_axis] = useState(4);
 
-    const [xDelta, setXDelta] = useState(0);
+    //Where the ball is heading
+    const [xDelta, setXDelta] = useState(1);
     const [yDelta, setYDelta] = useState(1);
 
     //Game loop
     useEffect(()=>{
         //Returns a new axis dependent on delta
         const returnNewAxis = (prevAx, setDeltaFunc, delta, axisType) => {
-            console.log("---", axisType)
-            console.log("Axis ", axisType, ": ", prevAx)
-            console.log("Delta ", axisType, ": ", delta)
-            if(prevAx > 7 || prevAx < 0){
-                setDeltaFunc(prevDelta => prevDelta*-1)
+            if(prevAx >= 7 || prevAx <= 0){
+                setDeltaFunc(prevDelta => {
+                    if((prevAx >= 7 && prevDelta>0) || (prevAx <= 0 && prevDelta<0)){
+                        return prevDelta*-1
+                    }else{
+                        return prevDelta
+                    }
+                })
                 return prevAx-delta
             }else{
                 return prevAx+delta
@@ -31,15 +35,17 @@ function App() {
         const interval = setInterval(()=>{
             setX_axis(prevXAx => returnNewAxis(prevXAx, setXDelta, xDelta, "X"))
             setY_axis(prevYAx => returnNewAxis(prevYAx, setYDelta, yDelta, "Y"))
-        }, 3000)
-        return ()=> {clearInterval(interval)}
+        }, 250)
+        return ()=> {
+            clearInterval(interval)
+        }
     }, [xDelta, yDelta])
 
     let gameBoard = []
     for (let y = 0; y < y_length; y++) {
         let row = []
         for (let x = 0; x < x_length; x++) {
-            const active = y===y_axis && x === x_axis;
+            const active = y===Math.floor(y_axis) && x === Math.floor(x_axis);
             row.push(<Celle active={active} key={`${x}-${y}`}/>)
         }
         gameBoard.push(<div className="row" key={y}>{row}</div>)
